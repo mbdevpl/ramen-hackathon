40aafda2b67eae2fff031c3f82ad45457efe1c72
KAZUYA IKOMA <audu817@gmail.com>
2018-06-10 01:35:39 +0900

    fix typo

--- app/src/main/java/com/example/kazuya/jiro/FirstFragment.kt
+++ app/src/main/java/com/example/kazuya/jiro/FirstFragment.kt
@@ -1,6 +1,5 @@
 package com.example.kazuya.jiro
 
-import android.opengl.Visibility
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.view.ViewGroup
@@ -8,73 +7,72 @@ import android.view.LayoutInflater
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.View
-import kotlinx.android.synthetic.main.fragment_first.*
 import kotlinx.android.synthetic.main.fragment_first.view.*
 import java.util.*
 
 
 class FirstFragment : Fragment(), TextToSpeech.OnInitListener {
-    private var tts : TextToSpeech? = null
+    private var tts: TextToSpeech? = null
 
     private var baseCount = 0
     private var baseVegCount = 0
-    private var gerlicCount = 0
+    private var garlicCount = 0
     private var porkCount = 0
     private var oilCount = 0
     private var eggCount = 0
 
-    private var nudleText = ""
+    private var noodleText = ""
     private var vegText = ""
-    private var gerlicText = ""
+    private var garlicText = ""
     private var oilText = ""
 
-    private var speechText  = ""
+    private var speechText = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TextToSpeech初期化
+        // TextToSpeech init
         tts = TextToSpeech(context, this)
-//        speech_ja.setOnClickListener(this)
-//        speech_en.setOnClickListener(this)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_first, null)
-        // ラーメン
+        // Ramen
         view.ramenButton.setOnClickListener {
             when (baseCount) {
                 0 -> {
                     view.baseImage.setImageDrawable(resources.getDrawable(R.drawable.ramens))
-                    baseCount ++
-                    nudleText = "麺少なめ"
+                    baseCount++
+                    noodleText = "麺少なめ"
                 }
                 1 -> {
                     view.baseImage.setImageDrawable(resources.getDrawable(R.drawable.ramenm))
-                    baseCount ++
-                    nudleText = ""
+                    baseCount++
+                    noodleText = ""
                 }
                 2 -> {
                     view.baseImage.setImageDrawable(resources.getDrawable(R.drawable.ramenl))
                     baseCount = 0
                 }
-                else -> {baseCount = 0}
+                else -> {
+                    baseCount = 0
+                }
 
             }
-            speechText = nudleText + vegText + gerlicText + oilText
+            speechText = noodleText + vegText + garlicText + oilText
             view.textView.text = speechText
         }
 
-        // ヤサイ
+        // Vegetable
         view.vegButton.setOnClickListener {
             when (baseVegCount) {
                 0 -> {
                     view.baseVegImage.setImageDrawable(resources.getDrawable(R.drawable.vegm))
-                    baseVegCount ++
+                    baseVegCount++
                     vegText = "やさい"
                 }
                 1 -> {
                     view.baseVegImage.setImageDrawable(resources.getDrawable(R.drawable.vegl))
-                    baseVegCount ++
+                    baseVegCount++
                     vegText = "やさいおおめ"
                 }
                 2 -> {
@@ -82,67 +80,73 @@ class FirstFragment : Fragment(), TextToSpeech.OnInitListener {
                     baseVegCount = 0
                     vegText = "やさいすくなめ"
                 }
-                else -> {baseVegCount = 0}
+                else -> {
+                    baseVegCount = 0
+                }
             }
-            speechText = nudleText + vegText + gerlicText + oilText
+            speechText = noodleText + vegText + garlicText + oilText
             view.textView.text = speechText
         }
 
-        // にんにく
+        // Garlic
         view.gerlicButton.setOnClickListener {
-            when (gerlicCount) {
+            when (garlicCount) {
                 0 -> {
                     view.garlicImage.visibility = View.VISIBLE
                     view.garlicImage.setImageDrawable(resources.getDrawable(R.drawable.garlics))
-                    gerlicCount ++
-                    gerlicText = "にんにくすくなめ"
+                    garlicCount++
+                    garlicText = "にんにくすくなめ"
                 }
                 1 -> {
                     view.garlicImage.setImageDrawable(resources.getDrawable(R.drawable.garlicm))
-                    gerlicCount ++
-                    gerlicText = "にんにく"
+                    garlicCount++
+                    garlicText = "にんにく"
                 }
                 2 -> {
                     view.garlicImage.setImageDrawable(resources.getDrawable(R.drawable.garlicl))
-                    gerlicCount ++
-                    gerlicText = "にんにくおおめ"
+                    garlicCount++
+                    garlicText = "にんにくおおめ"
                 }
                 3 -> {
                     view.garlicImage.setImageDrawable(resources.getDrawable(R.drawable.garlics))
                     view.garlicImage.visibility = View.GONE
-                    gerlicCount = 0
-                    gerlicText = ""
+                    garlicCount = 0
+                    garlicText = ""
                 }
-                else -> {baseVegCount = 0}
+                else -> {
+                    baseVegCount = 0
+                }
 
             }
-            speechText = nudleText + vegText + gerlicText + oilText
+            speechText = noodleText + vegText + garlicText + oilText
             view.textView.text = speechText
         }
 
-        // ぶた
+        // Pork
         view.porkButton.setOnClickListener {
             when (porkCount) {
                 0 -> {
                     view.porkImage.setImageDrawable(resources.getDrawable(R.drawable.pork2))
-                    porkCount ++
+                    porkCount++
                 }
                 1 -> {
                     view.porkImage.setImageDrawable(resources.getDrawable(R.drawable.pork4))
-                    porkCount ++
+                    porkCount++
                 }
                 2 -> {
                     view.porkImage.setImageDrawable(resources.getDrawable(R.drawable.pork1))
                     porkCount = 0
                 }
-                else -> {porkCount = 0}
+                else -> {
+                    porkCount = 0
+                }
 
             }
-            speechText = nudleText + vegText + gerlicText + oilText
+            speechText = noodleText + vegText + garlicText + oilText
             view.textView.text = speechText
         }
 
-        // あぶら
+        // Fat
         view.oilButton.setOnClickListener {
             when (oilCount) {
                 0 -> {
@@ -155,13 +159,15 @@ class FirstFragment : Fragment(), TextToSpeech.OnInitListener {
                     oilCount = 0
                     oilText = ""
                 }
-                else -> {oilCount = 0}
+                else -> {
+                    oilCount = 0
+                }
             }
-            speechText = nudleText + vegText + gerlicText + oilText
+            speechText = noodleText + vegText + garlicText + oilText
             view.textView.text = speechText
         }
 
-        // たまご
+        // Egg
         view.eggButton.setOnClickListener {
             when (eggCount) {
                 0 -> {
@@ -172,16 +178,18 @@ class FirstFragment : Fragment(), TextToSpeech.OnInitListener {
                     view.eggImage.visibility = View.GONE
                     eggCount = 0
                 }
-                else -> {eggCount = 0}
+                else -> {
+                    eggCount = 0
+                }
 
             }
-            speechText = nudleText + vegText + gerlicText + oilText
+            speechText = noodleText + vegText + garlicText + oilText
             view.textView.text = speechText
         }
 
         view.callButton.setOnClickListener {
-            // 例: 麺少なめヤサイにんにくあぶら！
-            speechText = nudleText + vegText + gerlicText + oilText
+            // ex: men sukuname yasai oome abura (this is Jiro Ramen's Call)
+            speechText = noodleText + vegText + garlicText + oilText
             speakText(speechText)
         }
 
@@ -195,13 +203,12 @@ class FirstFragment : Fragment(), TextToSpeech.OnInitListener {
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            Log.d("TTS", "TextToSpeechが初期化されました。")
+            Log.d("TTS", "TextToSpeech Init")
 
-            // 音声再生のイベントリスナを登録
-            val listener : SpeechListener? = SpeechListener()
+            val listener: SpeechListener? = SpeechListener()
             tts!!.setOnUtteranceProgressListener(listener)
         } else {
-            Log.e("TTS", "TextToSpeechの初期化に失敗しました。")
+            Log.e("TTS", "TextToSpeech Init Failed")
         }
     }
 }
