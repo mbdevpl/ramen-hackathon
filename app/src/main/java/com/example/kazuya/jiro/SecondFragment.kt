be3aa5b549330e1316e9cd1bb4be8b210fd07194
KAZUYA IKOMA <audu817@gmail.com>
2018-06-10 03:44:02 +0900

    fix SecondFragment

--- app/src/main/java/com/example/kazuya/jiro/SecondFragment.kt
+++ app/src/main/java/com/example/kazuya/jiro/SecondFragment.kt
@@ -5,17 +5,30 @@ import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.support.v4.app.Fragment
 import android.view.View
-
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class SecondFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         print("hello")
+        // TODO: do something
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_second, null)
+        val view = inflater.inflate(R.layout.fragment_third, null)
+        val myWebView = view.findViewById<View>(R.id.ankiWebView) as WebView
+        myWebView.settings.javaScriptEnabled = true
+        CookieManager.getInstance().setAcceptCookie(true)
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                // return true //Indicates WebView to NOT load the url;
+                return false //Allow WebView to load url
+            }
+        }
+        myWebView.loadUrl("https://ankiweb.net/decks/")
         return view
     }
 }
