72e719a23b3fe8533dae37bcdcb92eba6be72623
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-06-09 18:08:53 +0900

    web view kinda works

--- app/src/main/java/com/example/kazuya/jiro/ThirdFragment.kt
+++ app/src/main/java/com/example/kazuya/jiro/ThirdFragment.kt
@@ -5,7 +5,9 @@ import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.support.v4.app.Fragment
 import android.view.View
-
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class ThirdFragment : Fragment() {
 
@@ -16,6 +18,16 @@ class ThirdFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_third, null)
+        val myWebView = view.findViewById<View>(R.id.ankiWebView) as WebView
+        myWebView.getSettings().setJavaScriptEnabled(true)
+        CookieManager.getInstance().setAcceptCookie(true)
+        myWebView.setWebViewClient(object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                // return true //Indicates WebView to NOT load the url;
+                return false //Allow WebView to load url
+            }
+        })
+        myWebView.loadUrl("https://ankiweb.net/decks/")
         return view
     }
 }
